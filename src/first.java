16255c925f1edbe8d977f7ba104e614ad4ddc626
Saurabh <saurabhb5021@gmail.com>
2023-10-20 22:56:00 +0530

    Initial commit

---
+++ src/first.java
@@ -0,0 +1,32 @@
+import java.sql.*;
+
+
+public class first {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        String url ="jdbc:mysql://localhost:3306/student";
+        String un = "root";
+        String pass = "12345678";
+        String query = "Select name from stud where id=3";
+
+
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con = DriverManager.getConnection(url,un,pass);
+            Statement st = con.createStatement();
+            ResultSet resultSet = st.executeQuery(query);
+            resultSet.next();
+            String name = resultSet.getString("name");
+            System.out.println(name);
+
+            st.close();
+            con.close();
+
+
+
+
+
+
+
+
+    }
+}
