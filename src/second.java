a4519cec65073eb9cccd4ddde761d6e929c97fa0
Saurabh <saurabhb5021@gmail.com>
2023-10-20 23:05:19 +0530

    Second.java

---
+++ src/second.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+public class second {
+    public static void main(String[] args)throws ClassNotFoundException, SQLException {
+        String url ="jdbc:mysql://localhost:3306/student";
+        String un = "root";
+        String pass = "12345678";
+        String query = "select * from stud";
+
+
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(url,un,pass);
+        Statement st = con.createStatement();
+        ResultSet resultSet = st.executeQuery(query);
+
+        String userData ="";
+        while(resultSet.next()){
+            userData = resultSet.getInt(1) + " " + resultSet.getString(2);
+            System.out.println(userData);
+        }
+        st.close();
+        con.close();
+    }
+}
